e7ca450d70c7f0e293cf22b276464ba70e1eabc1
Torman <tormantec@outlook.com>
2024-12-31 11:24:43 -0300

    fix: refactor customer microservice and geral

--- Api.Customer/Program.cs
+++ Api.Customer/Program.cs
@@ -1,44 +1,172 @@
+using Core.Wrappers;
+using Data.Interfaces;
+using Data.Ioc;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Core.Entities.Domain.User;
+using Core.ViewModel.User;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+    {
+        Description = "api key.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "basic"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "basic"
+                },
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Customer Web Api", Version = "v1" });
+});
+
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureServices(x => x.AddAutofac()).UseServiceProviderFactory(new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new AutofacDataModule());
+});
+builder.Host.ConfigureServices(x => x.AddAutofac()).UseServiceProviderFactory(new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new AutofacPersistanceModule());
+});
+
+builder.Services.AddSingleton<Service.Middleware.AuthorizationMiddleware>();
 
 var app = builder.Build();
+var auth = app.Configuration;
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseDeveloperExceptionPage();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer Web Api"));
+
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+/*role*/
+app.MapGet("/api/Role/Get/{id}", Results<Ok<Role>, NotFound> (IRoleService setService, int id) =>
+        setService.Get(id) is { } post
+            ? TypedResults.Ok(post)
+            : TypedResults.NotFound());
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/api/Role/GetALl",
+    async (IRoleService setService, [FromQuery] int? PageNumber, [FromQuery] int? PageSize) =>
+    {
+        try
+        {
+            var paginationFilter = new PaginationFilter();
+            if (PageNumber.HasValue && PageNumber > 0 && PageSize.HasValue && PageSize > 0)
+                paginationFilter = new PaginationFilter((int)PageNumber, (int)PageSize);
+
+            if ((PageNumber.HasValue && PageNumber > 0) && (PageSize == null || PageSize == 0))
+                paginationFilter = new PaginationFilter((int)PageNumber, 10);
+
+            var result = setService.Search(paginationFilter);
+
+            if (result.Data is null || result.Data.Count == 0)
+                return Results.NotFound("No Role posts found matching the filter.");
+
+            return Results.Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+        }
+    });
+
+app.MapGet("/api/Role/Search",
+    async ([FromQuery] string filterText, [FromQuery] int? PageNumber, [FromQuery] int? PageSize, [FromServices] IRoleService setService) =>
+    {
+        try
+        {
+            var paginationFilter = new PaginationFilter();
+            if (PageNumber.HasValue && PageNumber > 0 && PageSize.HasValue && PageSize > 0)
+                paginationFilter = new PaginationFilter((int)PageNumber, (int)PageSize, filterText);
+
+            if ((PageNumber.HasValue && PageNumber > 0) && (PageSize == null || PageSize == 0))
+                paginationFilter = new PaginationFilter((int)PageNumber, 10, filterText);
+
+            if (!string.IsNullOrEmpty(filterText))
+                paginationFilter.SerachText = filterText;
+
+            var result = setService.Search(paginationFilter);
+            if (result.Data is null || result.Data.Count == 0)
+                return Results.NotFound("No blog posts found matching the filter.");
+
+            return TypedResults.Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+        }
+    });
+
+app.MapPost("/api/Role/Add", async (IRoleService setService, [FromBody] RoleVM body) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    try
+    {
+        var serialized = JsonConvert.SerializeObject(body);
+        List<string> validation = setService.GetValidation(body).ToList();
+        if (validation is not null && validation.Count() > 0)
+            return TypedResults.Problem(JsonConvert.SerializeObject(validation));
+
+        var insert = setService.Add(JsonConvert.DeserializeObject<Role>(serialized));
+        return insert is not null
+        ? TypedResults.Ok(insert)
+        : TypedResults.NotFound();
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+    }
+});
+
+
+app.MapPost("/api/Role/update", async (IRoleService setService, [FromBody] RoleVM body) =>
+{
+    try
+    {
+
+        var serialized = JsonConvert.SerializeObject(body);
+        List<string> validation = setService.GetValidation(body).ToList();
+        if (validation is not null && validation.Count() > 0)
+            return TypedResults.Problem(JsonConvert.SerializeObject(validation));
+
+        var insert = setService.Update(JsonConvert.DeserializeObject<Role>(serialized));
+        return insert is not false
+        ? TypedResults.Ok(insert)
+        : TypedResults.NotFound();
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+    }
+});
+
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
