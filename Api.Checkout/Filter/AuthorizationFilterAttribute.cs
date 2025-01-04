7852e1f4cad90873efea3fc4f1e55aeae5ff9ac3
Torman <tormantec@outlook.com>
2025-01-04 11:37:31 -0300

    fix: update tosta notification

--- Api.Checkout/Filter/AuthorizationFilterAttribute.cs
+++ Api.Checkout/Filter/AuthorizationFilterAttribute.cs
@@ -1,23 +1,26 @@
-﻿using Service.Interfaces;
+﻿
+using Service.Interfaces;
 using System.Net;
 
-namespace Service.Filter
+namespace Api.Checkout.Filter
 {
     public class AuthorizationLoggedActionFilter : IEndpointFilter
     {
-        private readonly IAuthService authService;
-        public AuthorizationLoggedActionFilter(IAuthService authService)
-        {
-            this.authService = authService;
-        }
+        //private readonly IAuthService authService;
+        //public AuthorizationLoggedActionFilter(IAuthService authService)
+        //{
+        //    this.authService = authService;
+        //}
 
         public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
         {
+            var authService = context.HttpContext.RequestServices.GetRequiredService<IAuthService>();
             var auth_validate = authService.LoginValidate(context.HttpContext.Request.Headers["Authorization"].ToString());
             if (auth_validate.Token is null && !auth_validate.Logged)
                 context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;                
             
              return await next(context);
         }
+
     }    
 }
--- Api.Checkout/Program.cs
+++ Api.Checkout/Program.cs
@@ -4,10 +4,12 @@ using Autofac.Extensions.DependencyInjection;
 using Autofac;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
-using Service.Filter;
 using Service.Interfaces;
 using Core.Entities.Domain.Checkout;
 using Core.Entities.Domain.User;
+using Api.Checkout.Filter;
+using Core.Entities.Domain.Blog;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -69,33 +71,32 @@ app.UseHttpsRedirection();
 
 
 
-app.MapPost("/api/Checkout/add/{idProduto:int}", async (HttpRequest request,
+app.MapPost("/api/Checkout/add/{idProduto:int}", async Task<Results<Ok, NotFound, ProblemHttpResult>>  (HttpRequest request,
     ICheckoutService setService, IUserService userService,
     int idProduto, [FromQuery] int qtd) =>
-{
-    try
     {
-        var token = string.Empty;
-        var user = new User();
-        if (request.Headers.TryGetValue("Authorization", out var authorizationHeader))
+        try
         {
-            token = authorizationHeader.ToString();
-            if (!string.IsNullOrEmpty(token))
+            var token = string.Empty;
+            var user = new User();
+            if (request.Headers.TryGetValue("Authorization", out var authorizationHeader))
             {
-                user = await userService.GetCurrentUserByToken(token);
+                token = authorizationHeader.ToString();
+                if (!string.IsNullOrEmpty(token))
+                {
+                    user = await userService.GetCurrentUserByToken(token);
+                }
             }
-        }        
 
-        var item = new ShoppingCartItem(){ Quantity = qtd, ProductId = idProduto, UserId = user.Id, TotalPrice = 100 };
-        item = setService.Add(item);
-        return TypedResults.Ok();
-    }
-    catch (Exception ex)
-    {
-        return Results.Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
-    }
-}).AddEndpointFilter<AuthorizationLoggedActionFilter>();
-
+            var item = new ShoppingCartItem(){ Quantity = qtd, ProductId = idProduto, UserId = user.Id, TotalPrice = 100 };
+            item = setService.Add(item);
+            return TypedResults.Ok();
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+        }
+    }).AddEndpointFilter<AuthorizationLoggedActionFilter>();
 
 app.Run();
 
